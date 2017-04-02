169610feb84ee160006dee35700e8e6a55ddd845
Blake Shaw <blake.shaw@gmail.com>
2017-04-02 15:50:55 -0600

    Initial implementation of prime finder.

--- Primes/Program.cs
+++ Primes/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Primes
 {
@@ -10,6 +7,23 @@ namespace Primes
 	{
 		static void Main(string[] args)
 		{
+			int count = 1_000_000;
+			Console.WriteLine($"Count of first {count} primes: {FindPrimes(count).Length}");
+		}
+
+		static int[] FindPrimes(int max)
+		{
+			if (max < 2) return new int[0];
+			bool[] notPrime = new bool[max + 1];
+			notPrime[0] = true;
+			notPrime[1] = true;
+			for(int i = 2; i <= max; i++)
+			{
+				if (notPrime[i]) continue;
+				for(int j = i + i; j <= max; j += i)
+					notPrime[j] = true;
+			}
+			return notPrime.Where(x => !x).Select((x,i) => i).ToArray();
 		}
 	}
 }
